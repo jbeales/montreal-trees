a90b817458231af864a550f304339a7a15bd8c5d
John Beales <john@johnbeales.com>
2021-10-15 21:46:48 +0000

    Update tree data URL

--- app/Console/Commands/UpdateTreeDatabase.php
+++ app/Console/Commands/UpdateTreeDatabase.php
@@ -44,7 +44,7 @@ class UpdateTreeDatabase extends Command
         $url = $this->argument('csvUrl');
 
         if(empty($url)) {
-            $url = 'https://data.montreal.ca/dataset/   b89fd27d-4b49-461b-8e54-fa2b34a628c4/resource/64e28fe6-ef37-437a-972d-d1d3f1f7d891/download/arbres-publics.csv';
+            $url = 'https://data.montreal.ca/dataset/b89fd27d-4b49-461b-8e54-fa2b34a628c4/resource/64e28fe6-ef37-437a-972d-d1d3f1f7d891/download/arbres-publics.csv';
         }
 
 
